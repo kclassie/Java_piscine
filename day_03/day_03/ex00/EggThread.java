60ccbfcc68037b6e7f888e31e7eb18d8b97e6e0d
kclassie <aenigma07@mail.ru>
2022-04-22 00:35:50 +0300

    ex00 is ready

---
+++ day_03/day_03/ex00/EggThread.java
@@ -0,0 +1,14 @@
+public class EggThread implements Runnable {
+    int count;
+
+    public EggThread(int count) {
+
+        this.count = count;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < this.count; i++)
+            System.out.println("Egg");
+    }
+}
---
+++ day_03/day_03/ex00/HenThread.java
@@ -0,0 +1,14 @@
+public class HenThread implements Runnable {
+    int count;
+
+    public HenThread(int count) {
+
+        this.count = count;
+    }
+
+    @Override
+    public void run() {
+        for (int i = 0; i < this.count; i++)
+            System.out.println("Hen");
+    }
+}
